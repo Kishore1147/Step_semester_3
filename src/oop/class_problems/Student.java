1e1f474b03913a4814c2a5b9301b24507357acaf
Kishore114 <ka6311@srmist.edu.in>
2026-08-23 21:34:34 +0530

    Add Student class problem

---
+++ src/oop/class_problems/Student.java
@@ -0,0 +1,28 @@
+package oop.class_problems;
+
+public class Student {
+
+    String name;
+    int attendance;
+    static String collegeName = "SRM Institute os Science and Technology";
+    static int studentCount = 0;
+
+    public Student(String name, int attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    static void printCollegeInfo(){
+        System.out.println(collegeName);
+        System.out.println("Student created: " + studentCount);
+    }
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Ravi", 80);
+        Student s2 = new Student("Anitha", 90);
+
+        Student.printCollegeInfo();
+    }
+}
